0b5f9a1966faba419b7e6637f0ad3c77d1198ce0
Hillkim <hillkim@protonmail.com>
2020-05-23 20:58:18 +0300

    :tada: Initial scaffold,add boostrap

---
+++ src/config.jsx
@@ -0,0 +1,23 @@
+export const UserID = "user_Your User Id";
+export const TemplateId = "template_Your Template";
+
+/**
+ * go to emailjs.com
+ 1. create account using your gmail using this link 
+  https://dashboard.emailjs.com/account/create
+
+ 2. set up your account using the gmail account recipient and sender
+
+ 3. paste this on your email template 
+            You got a new message from {{name}}:
+
+            Email Address: {{email}}
+            Phone Number: {{email}}
+
+            Message :
+
+            {{message}}
+
+            Best wishes
+            From CW
+ */
